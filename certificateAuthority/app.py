e9821155e0337b8172b2b510e6d8f25475d42ff3
Vrinda <talk2vrinda@gmail.com>
2021-11-08 01:53:40 +0530

    'K"

--- certificateAuthority/app.py
+++ certificateAuthority/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.after_request
 def after_request(response):
     header = response.headers
-    header['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    header['Access-Control-Allow-Origin'] = 'https://localhost:3000'
     header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     header['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET, POST, DELETE, PUT'
     return response
