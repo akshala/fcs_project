923ad11837e726326e4a91bb07fe8b83f2f1a3cf
akshala <akshala.bhatnagar@gmail.com>
2021-11-09 20:17:46 +0530

    Atleast 2 images

--- frontend/src/Components/Products/NewProduct.jsx
+++ frontend/src/Components/Products/NewProduct.jsx
@@ -1,5 +1,5 @@
-import { Button } from "@material-ui/core";
-import { Add, Delete} from "@material-ui/icons";
+import { Button, IconButton } from "@material-ui/core";
+import { Add, AddAPhoto, Delete, Remove} from "@material-ui/icons";
 import React from "react";
 import { withRouter } from 'react-router-dom';
 import { Alert } from '@mui/material';
@@ -17,8 +17,10 @@ class NewProduct extends React.Component {
             price: 0,
             alert_severity: null,
             alert_message: null, 
-            captcha: null
+            captcha: null,
+            number_of_images: 2
         }
+        this.images = [null, null]
         this.create = this.create.bind(this);
         this.discard = this.discard.bind(this);
         this.onChange = this.onChange.bind(this);
@@ -36,8 +38,7 @@ class NewProduct extends React.Component {
         data.append('description', this.state.description); 
         data.append('category', this.state.category)
         data.append('price', this.state.price)
-        data.append('image_1',  this.image1.files[0])
-        data.append('image_2', this.image2.files[0])
+        data.append('images',  this.images.map((image) => image?.files[0]))
         data.append('captcha', this.state.captcha)
         axios.post(`https://192.168.2.239:5000/products/new`, data,
         {
@@ -84,20 +85,24 @@ class NewProduct extends React.Component {
                 <span>Price: </span>
                 <input type="number" value={this.state.price} onChange={(event) => this.setState({price: event.target.value})} />
             </div>
-            <div className="ProductText">
-                <label>Image 1: </label>
-                <input ref={(ref) => { this.image1 = ref; }} type="file" id="file" accept=".png, .jpg" />
-            </div>
-            <div className="ProductText">
-                <label>Image 2: </label>
-                <input ref={(ref) => { this.image2 = ref; }} type="file" id="file" accept=".png, .jpg" />
-            </div>
+            {Array.from(Array(this.state.number_of_images).keys()).map((id) => <div className="ProductText">
+                <label>Image {id + 1}: </label>
+                <input ref={(ref) => { this.images[id] = ref; }} type="file" id="file" accept=".png, .jpg" />
+            </div>)}
+            <Button onClick={() => {this.setState({number_of_images: this.state.number_of_images + 1}, () => this.images.push(null));}} >
+                <AddAPhoto /> 
+                <span> Add more</span>
+            </Button>
+            <Button disabled={this.state.number_of_images <= 2} onClick={() => {this.setState({number_of_images: this.state.number_of_images - 1}, () => this.images.pop());}} >
+                <Remove /> 
+                <span> Remove</span>
+            </Button>
             <ReCAPTCHA
             sitekey="6LeC2RodAAAAAH0Ujxo7YdsISfdqnJ1F48sZQXdy"
             onChange={this.onChange}
             />
             <div className="Controls">
-            <Button disabled={!(this.state.name && this.categories.includes(this.state.category) && this.state.price > 0 && this.image1 && this.image2)} onClick = {this.create}>
+            <Button disabled={!(this.state.name && this.categories.includes(this.state.category) && this.state.price > 0 && this.state.number_of_images >= 2)} onClick = {this.create}>
                     <Add />
                     <span>Create</span>
                 </Button>
